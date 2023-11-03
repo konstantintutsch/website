ab338c1bf486c11a465f1f9fbe3d747bbdec5b2b
Konstantin Tutsch <mail@konstantintutsch.com>
2023-11-03 18:10:14 +0100

    Add development scripts

---
+++ _development/new-post.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [[ "${PWD##*/}" != "Website" ]]
+then
+    echo "Not in root of the website's source. Exiting …"
+    exit 1
+fi
+
+# All available tags
+TAGS=("layout" "image" "title" "description" "tags" "time")
+VALUES=("post" ""      ""      ""            ""     "$(date +"%H:%M:%S %z")")
+
+# Set values of tags if not already set
+for i in ${!TAGS[@]}
+do
+    if [[ ${VALUES[$i]} == "" ]]
+    then
+        read -p "${TAGS[$i]}: " "VALUES[$i]"
+    fi
+done
+
+# define filename with slugified title
+FILE="./_posts/$(date +%Y-%m-%d)-$(echo ${VALUES[2]} | tr -dc "[:alnum:]\n\r\ " | tr "[:upper:]" "[:lower:]" | tr "\ " "-" | sed "s/--/-/g" ).md"
+appendf() {
+    echo "$1" >> "$FILE"
+}
+
+# create file with tags and values
+touch "$FILE"
+appendf "---"
+for i in ${!TAGS[@]}
+do
+    appendf "${TAGS[$i]}: ${VALUES[$i]}"
+done
+appendf "---"
+
+# enter editing and wait for deploying, testing or deleting
+DEPLOY=""
+while [[ ${DEPLOY} != "deploy" ]]
+do
+    ${EDITOR} ${FILE}
+    read -p "Dou you want to [deploy/test/delete] your site?: " "DEPLOY"
+    if [[ ${DEPLOY} == "test" ]]
+    then
+        jekyll serve
+    elif [[ ${DEPLOY} == "delete" ]]
+    then
+        rm "${FILE}"
+        exit
+    fi
+done
+
+# deploy new post with git post-commit hook
+git add "${FILE}"
+git commit -m "New post: ${VALUES[2]}"
+git push -u origin main
---
+++ _development/post-commit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -i
+
+# .git/hooks/post-commit (770):
+#
+# #!/bin/bash
+#
+# export SFTPUN_STRATO="username"
+# export SFTPPW_STRATO="password"
+#
+# ./_development/post-commit.sh
+#
+
+DOMAIN="konstantintutsch.de"
+
+USERNAME="${SFTPUN_STRATO}"
+PASSWORD="${SFTPPW_STRATO}" 
+
+JEKYLL_ENV=production jekyll build
+
+cd "./_site"
+
+if [ ! -d "${HOME}/.mnt/website" ]
+then
+    mkdir -p "${HOME}/.mnt/website"
+fi
+sshfs -o reconnect,password_stdin "${USERNAME}@ssh.strato.de:/${DOMAIN}" "${HOME}/.mnt/website" <<< "$PASSWORD"
+rsync -rvz --progress --delete ./ "${HOME}/.mnt/website"
+umount "${HOME}/.mnt/website"
+
+if [[ "${PWD##*/}" == "_site" ]]
+then 
+    cd ..
+fi
