265675edfdfc177713d00824cc7138e0ccf93f03
Konstantin Tutsch <mail@konstantintutsch.com>
2023-11-11 11:58:05 +0100

    Use latest post for <lastmod> of Blog and Archive

--- _plugins/last-modified.rb
+++ _plugins/last-modified.rb
@@ -1,13 +1,11 @@
 def apply_modified(page)
-  if page.data['title'] == "Blog" ||page.data['title'] == "Archive"
-    modification_time = DateTime.now.strftime("%Y-%m-%d %H:%M:%S %z")
-  else
+  unless page.data['title'] == "Blog" ||page.data['title'] == "Archive"
     # get last modified from file system
     modification_time = File.mtime page.path
   end
 
   # insert into page.last-modified
-  page.data['last-modified'] = modification_time
+  page.data['last_modified'] = modification_time
 end
 
 Jekyll::Hooks.register :pages, :pre_render do |page|
@@ -16,5 +14,3 @@ end
 Jekyll::Hooks.register :posts, :pre_render do |post|
   apply_modified post
 end
-
-
