0f7dbadff585656f02c2d96a788b310fe2602d31
Konstantin Tutsch <mail@konstantintutsch.com>
2024-01-28 21:26:15 +0100

    Move to VPS for hosting

--- _development/deploy.sh
+++ _development/deploy.sh
@@ -4,28 +4,16 @@
 #
 # #!/bin/bash
 #
-# export SFTPUN_STRATO="username"
-# export SFTPPW_STRATO="password"
-#
 # ./_development/deploy.sh
 #
 
 DOMAIN="konstantintutsch.com"
 
-USERNAME="${SFTPUN_STRATO}"
-PASSWORD="${SFTPPW_STRATO}" 
-
 JEKYLL_ENV=production jekyll build
 
 cd "./_site"
 
-if [ ! -d "${HOME}/.mnt/website" ]
-then
-    mkdir -p "${HOME}/.mnt/website"
-fi
-sshfs -o reconnect,password_stdin "${USERNAME}@ssh.strato.de:/${DOMAIN}" "${HOME}/.mnt/website" <<< "$PASSWORD"
-rsync -rvz --progress --delete ./ "${HOME}/.mnt/website"
-umount "${HOME}/.mnt/website"
+rsync -rvz --progress --delete ./ "root@${DOMAIN}:/var/www/${DOMAIN}"
 
 if [[ "${PWD##*/}" == "_site" ]]
 then 
