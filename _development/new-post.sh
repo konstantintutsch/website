e4131d258c72af96403af98e866e17708f7d487d
Konstantin Tutsch <mail@konstantintutsch.com>
2024-02-20 17:36:04 +0100

    Remove dash from hashtags in Fediverse announcements

--- _development/new-post.sh
+++ _development/new-post.sh
@@ -62,7 +62,7 @@ git add "./assets/images"
 git commit -m "New post: ${VALUES[0]}"
 git push -u origin main
 
-for TAG in ${VALUES[2]}; do printf -v HASHTAGS '%s#%s ' "${HASHTAGS}" "${TAG}"
+for TAG in ${VALUES[2]}; do HASHTAG="${TAG//-/}"; printf -v HASHTAGS '%s#%s ' "${HASHTAGS}" "${HASHTAG}"; done
 printf -v toot_content '%s #blog\n\n%s%s/\n\n%s' "${VALUES[0]}" "${PREPEND_URL}" "${URL}" "${HASHTAGS}"
 toot post "${toot_content}"
 
