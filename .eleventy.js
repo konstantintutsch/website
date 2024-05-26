43c3fd19f125b742c7a0fca7157983427e34ed77
Konstantin Tutsch <mail@konstantintutsch.com>
2024-05-26 14:42:29 +0200

    Generate favicon sizes with @11ty/eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 // Functions
 const dateConversion = require("./date.js");
 const stringFormatter = require("./formatter.js");
+const generalTools = require("./tools.js");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
@@ -9,6 +10,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addFilter("rfcDate", function(value) { return dateConversion.rfc822(value); });
     eleventyConfig.addFilter("isoDate", function(value) { return dateConversion.iso8601(value); })
 
+    eleventyConfig.addShortcode("transformImage", async function(input, output, sizes, formats) { return await generalTools.transformImage(input, output, sizes, formats); });
     eleventyConfig.addShortcode("social", function(id, name = "", classes = "", extra = "", tracking = "") { return stringFormatter.social(id, name, classes, extra, tracking); });
     eleventyConfig.addShortcode("image", function(file, description, caption, thumbnail = false) { return stringFormatter.image(file, description, caption, thumbnail); });
     eleventyConfig.addShortcode("post", function(post) { return stringFormatter.post(post); });
--- tools.js
+++ tools.js
@@ -1,8 +1,23 @@
+const pluginImage = require("@11ty/eleventy-img");
+
+// Site data
+const site = require("./src/_data/site.json");
+
 module.exports = {
     leadingZero: (number) => {
         number = number.toString();
         while (number.length < 2) number = "0" + number;
         while (number < 0 && number.length < 3) number = "-0" + (number * -1);
         return number;
+    },
+
+    transformImage: async (input, output, sizes, formats) => {
+        let metadata = await new pluginImage.Image(input, {
+            heights: sizes,
+            formats: formats,
+            urlPath: output,
+        });
+
+        return `<!-- ${metadata.src} transformed to ${metadata.options.formats} with height ${metadata.options.heights} -->\n`;
     },
 }
